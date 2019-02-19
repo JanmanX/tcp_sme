d037e355178f6257bf2b8fcc7742ba5285edb6bf
Jan Meznik <jan@meznik.dk>
2019-02-19 14:11:33 +0100

    Created template DotNet project.

---
+++ src/TCPIP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPIP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
