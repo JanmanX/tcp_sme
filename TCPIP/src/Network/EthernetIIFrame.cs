69432c1f33fa875e136a8988af07e41c59b89aac
Jan Meznik <jan@meznik.dk>
2019-03-06 11:06:11 +0100

    Added basic Ethernet II frame class.

---
+++ TCPIP/src/Network/EthernetIIFrame.cs
@@ -0,0 +1,20 @@
+using System;
+using SME;
+
+namespace TCPIP
+{
+    enum EtherType
+    {
+        IPv4 = 0x0800,
+        ARP = 0x0806,
+        IPv6 = 0x86DD,
+    }
+
+
+    class EthernetIIFrame
+    {
+
+    }
+
+
+}
