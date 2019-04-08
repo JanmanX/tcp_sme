46e1c15035074bec12c0c92faed61e0fe37ec55c
Mark Jan Jacobi <mark@jacobi.pm>
2019-04-08 21:04:56 +0200

    Added global macros to code, and actual implementation of logging, so the compiler does not complain before and after generation

--- TCPIP/src/Components/Logging.cs
+++ TCPIP/src/Components/Logging.cs
@@ -7,6 +7,22 @@ namespace TCPIP
 {
     class Logger{
         public static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
-       
+        // Template void functions, changed with the preprocessor
+        public static void TRACE(string x){log.Trace(x);}
+        public static void DEBUG(string x){log.Debug(x);}
+        public static void INFO(string x){log.Info(x);}
+        public static void WARN(string x){log.Warn(x);}
+        public static void ERROR(string x){log.Error(x);}
+        public static void FATAL(string x){log.Fatal(x);}
+    }
+class Logging{
+        public static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+        // Template void functions, changed with the preprocessor
+        public static void TRACE(string x){log.Trace(x);}
+        public static void DEBUG(string x){log.Debug(x);}
+        public static void INFO(string x){log.Info(x);}
+        public static void WARN(string x){log.Warn(x);}
+        public static void ERROR(string x){log.Error(x);}
+        public static void FATAL(string x){log.Fatal(x);}
     }
 }
---
+++ TCPIP/src/Components/macros.h
@@ -0,0 +1,8 @@
+// Defines the macros for the logging library
+#define Logger this
+#define TRACE(x) SimulationOnly(() =>{Logging.log.Trace(x);})
+#define DEBUG(x) SimulationOnly(() =>{Logging.log.Debug(x);})
+#define INFO(x) SimulationOnly(() =>{Logging.log.Info(x);})
+#define WARN(x) SimulationOnly(() =>{Logging.log.Warn(x);})
+#define ERROR(x) SimulationOnly(() =>{Logging.log.Error(x);})
+#define FATAL(x) SimulationOnly(() =>{Logging.log.Fatal(x);})
