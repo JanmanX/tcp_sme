b52010417f1c0d6a6595030758fb385ec6e3c1f8
Jan Meznik <jan@meznik.dk>
2019-04-17 09:52:54 +0200

    Added C TAP device. Pipe this to our simulator?

---
+++ TCPIP/src/Simulations/tap_test/tap_comm.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/if.h>                                                         
+#include <linux/if_tun.h> 
+#include <stdarg.h>                                                                
+#include <errno.h> 
+
+static int tun_fd;
+static char* dev;
+
+char *tapaddr = "10.0.0.5";
+char *taproute = "10.0.0.0/24";
+
+int run_cmd(char *cmd, ...)                                                        
+{                                                                                  
+    va_list ap;                                                                    
+
+    #define CMDBUFLEN 100
+    char buf[CMDBUFLEN];                                                           
+    va_start(ap, cmd);                                                             
+    vsnprintf(buf, CMDBUFLEN, cmd, ap);                                            
+                                                                                   
+    va_end(ap);                                                                    
+                                                                                  
+    return system(buf);                                                            
+}                                                                                  
+  
+
+static int set_if_route(char *dev, char *cidr)
+{
+    return run_cmd("ip route add dev %s %s", dev, cidr);
+}
+
+static int set_if_address(char *dev, char *cidr)
+{
+    return run_cmd("ip address add dev %s local %s", dev, cidr);
+}
+
+static int set_if_up(char *dev)
+{
+    return run_cmd("ip link set dev %s up", dev);
+}
+
+/*
+ * Taken from Kernel Documentation/networking/tuntap.txt
+ */
+static int tun_alloc(char *dev)
+{
+    struct ifreq ifr;
+    int fd, err;
+
+    if( (fd = open("/dev/net/tap", O_RDWR)) < 0 ) {
+        perror("Cannot open TUN/TAP dev\n"
+                    "Make sure one exists with " 
+                    "'$ mknod /dev/net/tap c 10 200'");
+        exit(1);
+    }
+
+#define CLEAR(x) memset(&(x), 0, sizeof(x))
+    CLEAR(ifr);
+
+    /* Flags: IFF_TUN   - TUN device (no Ethernet headers)
+     *        IFF_TAP   - TAP device
+     *
+     *        IFF_NO_PI - Do not provide packet information
+     */
+    ifr.ifr_flags = IFF_TAP | IFF_NO_PI;
+    if( *dev ) {
+        strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+    }
+
+    if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ){
+        perror("ERR: Could not ioctl tun");
+        close(fd);
+        return err;
+    }
+
+    strcpy(dev, ifr.ifr_name);
+    return fd;
+}
+
+int tun_read(char *buf, int len)
+{
+    return read(tun_fd, buf, len);
+}
+
+int tun_write(char *buf, int len)
+{
+    return write(tun_fd, buf, len);
+}
+
+void tun_init()
+{
+    dev = calloc(10, 1);
+    tun_fd = tun_alloc(dev);
+
+    if (set_if_up(dev) != 0) {
+        printf("ERROR when setting up if\n");
+    }
+
+    if (set_if_route(dev, taproute) != 0) {
+        printf("ERROR when setting route for if\n");
+    }
+
+    if (set_if_address(dev, tapaddr) != 0) {
+        printf("ERROR when setting addr for if\n");
+    }
+}
+
+void free_tun()
+{
+    free(dev);
+}
+
+int main(int argc, char **argv) {
+    tun_init();    
+
+#define BUFSZ 256
+    char buf[BUFSZ] = {0};
+    while(1) {
+        if(read(tun_fd, buf, BUFSZ-1) > 0) {
+            for (int i = 0; i < BUFSZ; i++)
+            {
+                printf("%02X", buf[i]);
+            }
+        }
+    }
+}
