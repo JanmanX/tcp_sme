c1e587c6d3c53ba62a44a1e97189b0e125cdc633
Mark Jan Jacobi <mark@jacobi.pm>
2019-05-22 11:15:55 +0200

    Program for testing ICMP echo flow

--- TCPIP/src/Program.cs
+++ TCPIP/src/Program.cs
@@ -11,13 +11,42 @@ namespace TCPIP
         {
             using (var sim = new Simulation())
             {
-                var _interface = new Interface();
+
+                //var mem = new TrueDualPortMemory<byte>(8192);
+                //var simulator = new DatagramInputSimulator("data/udp_25/");
+                // var simulator = new TUNSimulator();
+                //                var network = new NetworkReader(simulator.frameBus);
+                //var internet = new InternetIn(simulator.datagramBusIn);
+                //simulator.datagramBusInControl = internet.datagramBusInControl;
+
+                //var transport = new Transport(internet.segmentBusIn);
+                //var dataInReader = new DataInReader(transport.dataInBus);
+
+
+                // Graph simulator
+                int packet_out_mem_size = 8192;
+                var packet_out_mem = new TrueDualPortMemory<byte>(packet_out_mem_size);
+                var packet_out = new PacketOut(packet_out_mem,packet_out_mem_size);
+
+                var simulator = new GraphFileSimulator("data/icmp_data/");
+                var internetIn = new InternetIn(simulator.datagramBusIn,packet_out.bus_in_internet);
+                var internetOut = new InternetOut(simulator.datagramBusOut,
+                                                  packet_out.bus_out,
+                                                  packet_out.bus_out_control);
+                var transport = new Transport(internetIn.segmentBusIn,packet_out.bus_in_transport);
+
+                // Use fluent syntax to configure the simulator.
+                // The order does not matter, but `Run()` must be
+                // the last method called.
+
+                // The top-level input and outputs are exposed
+                // for interfacing with other VHDL code or board pins
 
                 sim
-                    .AddTopLevelOutputs(_interface.interfaceBusOut)
-                    .AddTopLevelInputs(_interface.interfaceBus)
+//                    .AddTopLevelOutputs(dataInReader.)
+                    .AddTopLevelInputs(simulator.datagramBusIn)
                     .BuildCSVFile()
-                    .BuildVHDL()
+                    //.BuildVHDL()
                     .Run();
 
                 // After `Run()` has been invoked the folder
