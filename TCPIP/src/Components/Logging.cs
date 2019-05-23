7ea12b8d1fc7c4a6714797ae46a9a79bfe499546
Mark Jan Jacobi <mark@jacobi.pm>
2019-05-23 20:42:19 +0200

    Make it easier to detect wrong usage of logging, and initial preperations for inline function macros

--- TCPIP/src/Components/Logging.cs
+++ TCPIP/src/Components/Logging.cs
@@ -8,18 +8,28 @@ namespace TCPIP
     // Is not actually used, but replaced by the preprocessor if used in *.cpp.cs files
     public class LOGGER
     {
+        static string err = "LOGGER (not to be confused with Logging) cannot be called at runtime. Check if the file ends with *.cpp.cs";
         private static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
-        public static void TRACE(string x) { log.Trace(x); }
-        public static void DEBUG(string x) { log.Debug(x); }
-        public static void INFO(string x) { log.Info(x); }
-        public static void WARN(string x) { log.Warn(x); }
-        public static void ERROR(string x) { log.Error(x); }
-        public static void FATAL(string x) { log.Fatal(x); }
+        public static void TRACE(string x) { throw new System.Exception(err); }
+        public static void DEBUG(string x) { throw new System.Exception(err); }
+        public static void INFO(string x) { throw new System.Exception(err); }
+        public static void WARN(string x) { throw new System.Exception(err); }
+        public static void ERROR(string x) { throw new System.Exception(err); }
+        public static void FATAL(string x) { throw new System.Exception(err); }
     }
     // Used for actual logging, simply a wrapper with another name to not
     // conflict with the preprosessor
     class Logging
     {
         public static NLog.Logger log = NLog.LogManager.GetCurrentClassLogger();
+    }
+    class GENERAL_TOOLS{
+        // All general macros are inline, where the first argument of the function
+        // is the variable in which the return is saved;
+        public static void GENERATE_CHECKSUM(ushort test)
+        {
+            throw new System.Exception("Not implemented yet");
+        }
+
     }
 }
