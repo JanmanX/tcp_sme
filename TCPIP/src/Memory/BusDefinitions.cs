0ffef4239e1a483bd700a3cd8827bf76453f39f8
Mark Jan Jacobi <mark@jacobi.pm>
2019-05-30 19:49:58 +0200

    Renamed packet memory bus to better reflect the data contained, and added fragment offset and ip_id for segmentation information

--- TCPIP/src/Memory/BusDefinitions.cs
+++ TCPIP/src/Memory/BusDefinitions.cs
@@ -2,18 +2,18 @@ using SME;
 
 namespace TCPIP
 {
-    public partial class PacketOut
+    public partial class Memory
     {
-        public interface PacketOutBus : IBus
+        public interface InternetPacketBus : IBus
         {
-            [InitialValue(false)]
-            bool active { get; set; }
             long frame_number { get; set; } // Increments so we can distinguish between new packages
             byte ip_protocol { get; set; }
             ulong ip_dst_addr_0 { get; set; }
             ulong ip_dst_addr_1 { get; set; }
             ulong ip_src_addr_0 { get; set; }
             ulong ip_src_addr_1 { get; set; }
+            uint ip_id { get; set; }
+            uint fragment_offset { get; set; }
             int data_length { get; set; } // the size we are writing currently
             byte data { get; set; } // The data needed
         }
