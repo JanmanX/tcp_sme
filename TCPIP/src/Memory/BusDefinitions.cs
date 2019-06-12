3eb3c4802d3829779d9a7a835db8b320a6b75ee6
Mark Jan Jacobi <mark@jacobi.pm>
2019-06-12 13:32:25 +0200

    Rollback to expanded bus interfaces, instead of interitence from same interface

--- TCPIP/src/Memory/BusDefinitions.cs
+++ TCPIP/src/Memory/BusDefinitions.cs
@@ -2,9 +2,74 @@ using SME;
 
 namespace TCPIP
 {
-    public partial class Memory
+    public partial class DataOut
     {
-        public interface InternetPacketBus : IBus
+        public interface ReadBus : IBus
+        {
+            int socket { get; set; }
+            byte data { get; set; }
+        }
+        public interface WriteBus : IBus
+        {
+            int data { get; set; }
+        }
+    }
+
+    public partial class DataIn
+    {
+        public interface WriteBus : IBus
+        {
+            int socket { get; set; }
+            uint tcp_seq { get; set; }
+            byte data { get; set; }
+            bool invalidate { get; set; }
+            int data_length { get; set; }
+        }
+        public interface ReadBus : IBus
+        {
+            byte data { get; set; }
+        }
+    }
+
+
+    public partial class PacketOut
+    {
+        public interface ReadBus : IBus
+        {
+            [InitialValue(0x00)]
+            long frame_number { get; set; } // Increments so we can distinguish between new packages
+
+            [InitialValue(0x00)]
+            byte ip_protocol { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_dst_addr_0 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_dst_addr_1 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_src_addr_0 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_src_addr_1 { get; set; }
+
+            [InitialValue(0x00)]
+            uint ip_id { get; set; }
+
+            [InitialValue(0x00)]
+            uint fragment_offset { get; set; }
+
+            [InitialValue(0x00)]
+            int data_length { get; set; } // the size we are writing currently
+
+            [InitialValue(0x00)]
+            byte data { get; set; } // The data needed
+
+            [InitialValue(-1)]
+            int addr { get; set; } // The data address(-1 if order is not important)
+        }
+        public interface WriteBus  : IBus
         {
             [InitialValue(0x00)]
             long frame_number { get; set; } // Increments so we can distinguish between new packages
@@ -41,47 +106,78 @@ namespace TCPIP
         }
     }
 
-
-    public partial class DataOut
-    {
-        public interface ReadBus : IBus
-        {
-            int socket { get; set; }
-            byte data { get; set; }
-        }
-        public interface WriteBus : IBus
-        {
-            int data { get; set; }
-        }
-    }
-
-    public partial class DataIn
-    {
-        public interface WriteBus : IBus
-        {
-            int socket { get; set; }
-            uint tcp_seq { get; set; }
-            byte data { get; set; }
-            bool invalidate { get; set; }
-            int data_length { get; set; }
-        }
-        public interface ReadBus : IBus
-        {
-            byte data { get; set; }
-        }
-    }
-
-
-    public partial class PacketOut
-    {
-        public interface ReadBus : Memory.InternetPacketBus{}
-        public interface WriteBus : Memory.InternetPacketBus{}
-    }
-
     public partial class PacketIn
     {
-        public interface WriteBus : Memory.InternetPacketBus{}
-        public interface ReadBus : Memory.InternetPacketBus{}
+        public interface WriteBus  : IBus
+        {
+            [InitialValue(0x00)]
+            long frame_number { get; set; } // Increments so we can distinguish between new packages
+
+            [InitialValue(0x00)]
+            byte ip_protocol { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_dst_addr_0 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_dst_addr_1 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_src_addr_0 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_src_addr_1 { get; set; }
+
+            [InitialValue(0x00)]
+            uint ip_id { get; set; }
+
+            [InitialValue(0x00)]
+            uint fragment_offset { get; set; }
+
+            [InitialValue(0x00)]
+            int data_length { get; set; } // the size we are writing currently
+
+            [InitialValue(0x00)]
+            byte data { get; set; } // The data needed
+
+            [InitialValue(-1)]
+            int addr { get; set; } // The data address(-1 if order is not important)
+        }
+        public interface ReadBus  : IBus
+        {
+            [InitialValue(0x00)]
+            long frame_number { get; set; } // Increments so we can distinguish between new packages
+
+            [InitialValue(0x00)]
+            byte ip_protocol { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_dst_addr_0 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_dst_addr_1 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_src_addr_0 { get; set; }
+
+            [InitialValue(0x00)]
+            ulong ip_src_addr_1 { get; set; }
+
+            [InitialValue(0x00)]
+            uint ip_id { get; set; }
+
+            [InitialValue(0x00)]
+            uint fragment_offset { get; set; }
+
+            [InitialValue(0x00)]
+            int data_length { get; set; } // the size we are writing currently
+
+            [InitialValue(0x00)]
+            byte data { get; set; } // The data needed
+
+            [InitialValue(-1)]
+            int addr { get; set; } // The data address(-1 if order is not important)
+        }
 
     }
 }
