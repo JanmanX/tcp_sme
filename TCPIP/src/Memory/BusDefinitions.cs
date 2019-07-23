12c2da2c56c9c61189f3c68c2106bf4cbd2ef978
Mark Jan Jacobi <mark@jacobi.pm>
2019-07-23 14:54:51 +0200

    Add default values for read an write bus in dataOut

--- TCPIP/src/Memory/BusDefinitions.cs
+++ TCPIP/src/Memory/BusDefinitions.cs
@@ -6,13 +6,17 @@ namespace TCPIP
     {
         public interface ReadBus : IBus
         {
+            [InitialValue(0x00)]
             int socket { get; set; }
+            [InitialValue(0xFF)]
             byte data { get; set; }
         }
         public interface WriteBus : IBus
         {
+            [InitialValue(0x00)]
             long frame_number { get; set; }
             int socket { get; set; }
+             [InitialValue(0xFF)]
             byte data { get; set; }
         }
     }
