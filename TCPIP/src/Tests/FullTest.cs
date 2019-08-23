789dcb00dd9521d43bc6cfa845bfc67a6b270dea
Mark Jan Jacobi <mark@jacobi.pm>
2019-08-23 04:15:08 +0200

    Use new big advanced udp test

--- TCPIP/src/Tests/FullTest.cs
+++ TCPIP/src/Tests/FullTest.cs
@@ -19,7 +19,8 @@ namespace TCPIP
 
                 // Graph simulator
                 //var simulator = new GraphFileSimulator("data/graphsimulation/udp_out_test/",450,true);
-                var simulator = new GraphFileSimulator("data/graphsimulation/advanced_udp_test/",1000000,true);
+                //var simulator = new GraphFileSimulator("data/graphsimulation/advanced_udp_test/",1000000,true);
+                var simulator = new GraphFileSimulator("data/graphsimulation/big_advanced_udp_test/",1000000000,true);
                 //var simulator = new GraphFileSimulator("data/graphsimulation/test/",1000000,true);
 
                 // Allocate memory blocks
