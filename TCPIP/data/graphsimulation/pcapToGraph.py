b795514ddbdd6a24b31385a50a9005c79a74338b
Mark Jan Jacobi <mark@jacobi.pm>
2019-08-26 17:23:11 +0200

    Added complex udp test, with both input and output tests

--- TCPIP/data/graphsimulation/pcapToGraph.py
+++ TCPIP/data/graphsimulation/pcapToGraph.py
@@ -13,15 +13,17 @@ parser.add_argument('dumptype',
                      const='raw',
                      nargs='?',
                      help='mode to dump the packets',
-                     choices=['raw', 'icmp_echo', 'send_all' , 'send_all_save_data'])
+                     choices=['raw', 'icmp_echo', 'send_all' , 'send_all_save_data', 'complex_udp_test'])
 args = parser.parse_args()
 
 # rdpcap comes from scapy and loads in our pcap file
 packets = rdpcap(args.inputfile)
 print("Doing mode:" + args.dumptype )
+print("Working....")
 last_end_packet = 0
 
 # Let's iterate through every packet
+#for counter, packet in enumerate(packets):
 for counter, packet in tqdm.tqdm(enumerate(packets)):
     data = bytes(packet)
 
@@ -88,3 +90,41 @@ for counter, packet in tqdm.tqdm(enumerate(packets)):
                 os.makedirs(os.path.dirname(filename), exist_ok=True)
                 with open(filename,"wb") as f:
                     f.write(packet[Raw].load)
+
+    if (args.dumptype == "complex_udp_test"):
+        # Socket indexer
+        soc = {6789 : 0,  # Good
+               6543 : 1,  # Good
+               3456 : 2 } # Bad
+        # zero extending
+
+        # save the packet files
+        if counter == 0:
+            endstr = ""
+        else:
+            endstr = "_" + str(counter) + "0"
+        filename = args.outputfolder + "/" + f"{counter + 1}0"+ endstr +"-send.bin"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        with open(filename,"wb") as f:
+                f.write(data)
+        # save data from the packet files
+        if UDP in packet:
+            if last_end_packet != 0:
+                filename1 = args.outputfolder + "/" + f"{counter + 1}1_" + str(last_end_packet) +f"1-datain.{soc[packet[UDP].dport]}.bin"
+                filename2 = args.outputfolder + "/" + f"{counter + 1}2_{counter + 1}1_" + str(last_end_packet) +f"2-dataout.{soc[packet[UDP].dport]}.bin"
+                filename3 = args.outputfolder + "/" + f"{counter + 1}3_" + str(last_end_packet) +f"3-receive.temporary_deleteme.bin"
+            else:
+                filename1 = args.outputfolder + "/" + f"{counter + 1}1-datain.{soc[packet[UDP].dport]}.bin"
+                filename2 = args.outputfolder + "/" + f"{counter + 1}2_{counter + 1}1-dataout.{soc[packet[UDP].dport]}.bin"
+                filename3 = args.outputfolder + "/" + f"{counter + 1}3-receive.temporary_deleteme.bin"
+
+            if not packet[Raw].load.startswith(b"!"):
+                last_end_packet = counter + 1
+                os.makedirs(os.path.dirname(filename1), exist_ok=True)
+                with open(filename1,"wb") as f:
+                    f.write(packet[Raw].load)
+                with open(filename2,"wb") as f:
+                    f.write(packet[Raw].load)
+                with open(filename3,"wb") as f:
+                    #f.write(packet[Raw].load)
+                    f.write(b"A"*50000)
