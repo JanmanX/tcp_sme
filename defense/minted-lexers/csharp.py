63796e7728c60d542d10ff7e6a855f681daff4b2
Mark Jan Jacobi <mark@jacobi.pm>
2019-09-10 10:23:14 +0200

    Added implementation to slides, and begun on evaluation

---
+++ defense/minted-lexers/csharp.py
@@ -0,0 +1,22 @@
+#from pygments.lexers.asm import CppLexer
+from pygments.lexers.dotnet import CSharpLexer
+from pygments.token import Name, Keyword
+
+class MDCSharpLexer(CSharpLexer):
+    name = 'MDCSharp'
+    aliases = ['mdcsharp']
+
+    info = [('MetaData',Name,Keyword.Type),
+            ('InterfaceData',Name,Keyword.Type)]
+
+
+    def get_tokens_unprocessed(self, text):
+        for index, token, value in CSharpLexer.get_tokens_unprocessed(self, text):
+            submitted = False
+            for (s,a,t) in self.info:
+                if token is a and value == s:
+                    submitted = True
+                    yield index, t, value
+                    break
+            if(submitted == False):
+                yield index, token, value
