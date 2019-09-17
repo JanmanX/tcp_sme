a570bbb4341582ae48a003500c9a035d8dfcd3bb
Mark Jan Jacobi <mark@jacobi.pm>
2019-09-17 09:27:36 +0200

    Added script to print dot files in neato format

---
+++ TCPIP/data/graphsimulation/neatoToImage.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+if [ -z "$1" ]; then
+    echo "Supply path argument"
+    exit 1
+fi
+
+neatoconvert () {
+    if [[ ! -e "$1.png" ]]
+    then
+        echo "Doing $1"
+        neato -Tpng $1 -o $1.png;
+        neato -Teps $1 -o $1.eps;
+    fi
+}
+
+pushd $1;
+export -f neatoconvert
+
+if [[ -e "out.mp4" ]]
+then
+    rm out.mp4
+fi
+
+SHELL=$(type -p bash) find . -name "*dot" | sort -n | parallel neatoconvert
+ffmpeg -r 10 -i %*.dot.png -r 10 out.mp4;
+
+#rm -- *.dot
+#rm -- *.dot.png
+
+popd;
